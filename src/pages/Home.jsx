8a55a786efe973042e88e00c247134b4e5d39e7c
unrecoveryloss <149214910+unrecoveryloss@users.noreply.github.com>
2025-06-23 02:49:05 -0400

    guia n3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,30 @@
-import CardProducto from '../components/CardProducto';
+import CardProducto from "../components/CardProducto";
 function Home() {
- return (
+  return (
     <div className="container mt-4">
-    <h1>Productos Disponibles</h1>
-    <CardProducto nombre="Pan Integral" precio="$500" />
+      <h1>Productos Disponibles</h1>
+      <CardProducto nombre="Pan Integral" precio="$500" />
     </div>
- );
+  );
 }
 export default Home;
 
 import CerrarSesion from "../components/CerrarSesion";
 function Home() {
-return (
-<div>
-<h2>Bienvenido a EcoFood</h2>
-<CerrarSesion />
-</div>
-);
+  return (
+    <div>
+      <h2>Bienvenido a EcoFood</h2>
+      <CerrarSesion />
+    </div>
+  );
 }
+
+import { getUserData } from "../services/userService";
+import { useAuth } from "../context/AuthContext";
+useEffect(() => {
+  const fetch = async () => {
+    const datos = await getUserData(user.uid);
+    setUserData(datos);
+  };
+  if (user) fetch();
+}, [user]);
