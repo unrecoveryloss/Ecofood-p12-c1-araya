4178cb0d1490abc1ab3c8c997b21f060965d6ce0
unrecoveryloss <149214910+unrecoveryloss@users.noreply.github.com>
2025-06-23 02:41:22 -0400

    guia n2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,3 +8,13 @@ function Home() {
  );
 }
 export default Home;
+
+import CerrarSesion from "../components/CerrarSesion";
+function Home() {
+return (
+<div>
+<h2>Bienvenido a EcoFood</h2>
+<CerrarSesion />
+</div>
+);
+}
