4c51ece59391928a1606a5576141bdd2391cc439
unrecoveryloss <149214910+unrecoveryloss@users.noreply.github.com>
2025-06-23 02:25:25 -0400

    guia n1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppRouter from './router';
+import AppRouter from './routes/AppRouter';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthProvider';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<AppRouter />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<BrowserRouter>
+<AuthProvider>
+<AppRouter />
+</AuthProvider>
+</BrowserRouter>
+</React.StrictMode>
+);
